e2954cd4a5bd1c419476af8f1e8b60b44db4555a
Aditya Devgan <adityadevgan1@gmail.com>
2020-01-21 02:29:05 +0530

    mem

---
+++ Stage 1 level 3 original/Assets/Scripts/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Timer : MonoBehaviour
+{
+    public float scoreAmount;
+    public float pointDecreasedPerSecond;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreAmount = 0f;
+        pointDecreasedPerSecond = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreAmount += pointDecreasedPerSecond * Time.deltaTime;
+    }
+}
